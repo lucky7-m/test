5116fa1f1809412134f2cab62eb9fe7b0110bb31
lucky7-m <58325282+lucky7-m@users.noreply.github.com>
2019-12-02 16:25:40 +0800

    Update test1.java

--- project1/src/package1/test1.java
+++ project1/src/package1/test1.java
@@ -2,9 +2,11 @@ package package1;
 
 public class test1 {
 	public static void main(String[] args) {
-		System.out.println("²âÊÔGitHub");
+		System.out.println("Â²Ã¢ÃŠÃ”GitHub");
 		System.out.println("2");
 		System.out.println("3");
 		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
 	}
 }
